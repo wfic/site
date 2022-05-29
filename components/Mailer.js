825e81f3764897b4dca97eaf124a3530dee43dea
ahadnur <ahadnur44@gmail.com>
2022-05-29 21:27:43 +0600

    All complete

---
+++ components/Mailer.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import emailjs from '@emailjs/browser'
+
+function Mailer() {
+  function sendMail(e){
+      e.preventDefault();
+      console.log("Clicked")
+
+      emailjs.sendForm(
+          'j6v86nq',
+          'j63vx7k',
+          e.target,
+          'QyF2vx7Ph2wC2G17t'
+      ).then(res => {
+          console.log(res);
+      }).catch(err => {
+          console.log(err);
+      })
+      e.reset()
+  }
+
+  return (
+    <div className="">
+        <h2 className="text-2xl font-semibold underline underline-offset-1 decoration-emerald-500 mb-8">Send your message to us..</h2>
+        <form className="max-w-[600px]" onSubmit={sendMail}>
+            <div className="flex flex-col mb-4">
+                <label className="text-md mb-1">Name</label>
+                <input type="text" name="name" className='border-2 border-gray-500 rounded px-2 py-2 outline-none' required/>
+            </div>
+            <div className="flex flex-col mb-4">
+                <label className="text-md mb-1">Email</label>
+                <input type="email" name="user_email" className='border-2 border-gray-500 rounded px-2 py-2 outline-none' required />
+            </div>
+            <div className="flex flex-col mb-4">
+                <label className="text-md mb-1">Message</label>
+                <textarea name="message" rows="4" className='border-2 border-gray-500 rounded px-2 py-2 outline-none' required ></textarea>
+            </div>
+            <button type="submit" className="bg-emerald-100 text-emerald-500 font-semibold px-8 py-2 rounded text-md">Send</button>
+        </form>
+    </div>
+  )
+}
+
+export default Mailer
--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
+
+import Mailer from '../components/Mailer'
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
 
@@ -20,17 +22,22 @@ function contact() {
                 </div>
             </div>
             <div className="container py-20">
-                <h3 className="text-3xl md:text-4xl font-Cinzel font-bold text-slate-800">Contact info</h3>
+                <h3 className="text-3xl md:text-4xl font-Cinzel font-bold text-slate-800 ">Contact info</h3>
 
-                <div className="mt-5">
-                    <h2 className="text-md md:text-xl font-semibold text-slate-800 py-2">Islamic Society of Wichita Falls</h2>
-                    <p className='text-md md:text-xl text-slate-800'>1701 Trigg Ln,</p>
-                    <p className='text-md md:text-xl text-slate-800'>Wichita Falls,</p>
-                    <p className='text-md md:text-xl text-slate-800 mb-3'>TX, 76308, USA</p>
-                    <div>
-                        <a href="tel:++19403375021" className='text-slate-800 mt-4 font-semibold text-md md:text-xl'>Phone: 19403375021</a>
+                <div className="grid grid-cols-1 md:grid-cols-2 mt-10">
+                    <div className="">
+                        <h2 className="text-md md:text-xl font-semibold text-slate-800 py-2">Islamic Society of Wichita Falls</h2>
+                        <p className='text-md md:text-xl text-slate-800'>1701 Trigg Ln,</p>
+                        <p className='text-md md:text-xl text-slate-800'>Wichita Falls,</p>
+                        <p className='text-md md:text-xl text-slate-800 mb-3'>TX, 76308, USA</p>
+                        <div>
+                            <a href="tel:++19403375021" className='text-slate-800 mt-4 font-semibold text-md md:text-xl'>Phone: 19403375021</a>
+                        </div>
+                        <div>
+                        </div>
                     </div>
                     <div>
+                        <Mailer />
                     </div>
                 </div>
             </div>
