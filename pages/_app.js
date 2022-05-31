144811f00bd7836d319bb6c2049645b4c8c772f7
ahadnur <ahadnur44@gmail.com>
2022-05-31 15:58:21 +0600

    gtag updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,23 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-
-import * as ga from '../lib/ga'
+import Script from 'next/script'
 import '../styles/globals.css'
 
 
 function MyApp({ Component, pageProps }) {
-    const router = useRouter()
-    useEffect(() => {
-        const handleRouteChange = (url) => {
-          ga.pageview(url)
-        }
-        //When the component is mounted, subscribe to router changes
-        //and log those page views
-        router.events.on('routeChangeComplete', handleRouteChange)
-    
-        // If the component is unmounted, unsubscribe
-        // from the event with the `off` method
-        return () => {
-          router.events.off('routeChangeComplete', handleRouteChange)
-        }
-      }, [router.events])
-    
     return (
-        <Component {...pageProps} />
+        <>
+            <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+            <Script strategy="afterInteractive">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    });
+                `}
+            </Script>
+            <Component {...pageProps} />
+        </>
     )
 }
 
